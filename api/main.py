3fb926ad825fa1204e8a2f1d7ace63a1176550bf
dudsaheb <dudsaheb@gmail.com>
2025-10-15 15:13:16 +0530

    m2

--- api/main.py
+++ api/main.py
@@ -23,48 +23,48 @@ app = FastAPI()
 # )
 
 
-@app.post("/populate/")
-def populate_db():
-    session = SessionLocal()
-    try:
-        # Insert Users
-        users = [User(username=f'user{i}', hashed_password=f'hash{i}', first_name=f'First{i}', last_name=f'Last{i}') for i in range(1, 31)]
-        session.add_all(users)
-        session.commit()
+# @app.post("/populate/")
+# def populate_db():
+#     session = SessionLocal()
+#     try:
+#         # Insert Users
+#         users = [User(username=f'user{i}', hashed_password=f'hash{i}', first_name=f'First{i}', last_name=f'Last{i}') for i in range(1, 31)]
+#         session.add_all(users)
+#         session.commit()
 
-        # Refresh each user instance if necessary
-        for user in users:
-            session.refresh(user)
+#         # Refresh each user instance if necessary
+#         for user in users:
+#             session.refresh(user)
 
-        # Insert Dogs, Posts, Comments, and Images
-        for user in users:
-            dogs = [Dog(name=f'Dog{j}', breed=f'Breed{j%5}', age=random.randint(1, 10), user_id=user.id) for j in range(1, 6)]
-            session.add_all(dogs)
+#         # Insert Dogs, Posts, Comments, and Images
+#         for user in users:
+#             dogs = [Dog(name=f'Dog{j}', breed=f'Breed{j%5}', age=random.randint(1, 10), user_id=user.id) for j in range(1, 6)]
+#             session.add_all(dogs)
             
-            posts = [Post(content=f'Content{k}', timestamp=datetime.now(timezone.utc), user_id=user.id) for k in range(1, 11)]
-            session.add_all(posts)
+#             posts = [Post(content=f'Content{k}', timestamp=datetime.now(timezone.utc), user_id=user.id) for k in range(1, 11)]
+#             session.add_all(posts)
 
-            # Insert an Image record with image set to None
-            image = Image(image=None, user_id=user.id)
-            session.add(image)
+#             # Insert an Image record with image set to None
+#             image = Image(image=None, user_id=user.id)
+#             session.add(image)
         
-        session.commit()
+#         session.commit()
 
-        # Collect all posts to randomly assign comments
-        all_posts = session.query(Post).all()
-        for user in users:
-            selected_posts = random.sample(all_posts, 4)
-            for post in selected_posts:
-                comment = Comment(content=f'Comment from user {user.id} on post {post.id}', timestamp=datetime.now(timezone.utc), user_id=user.id, post_id=post.id)
-                session.add(comment)
+#         # Collect all posts to randomly assign comments
+#         all_posts = session.query(Post).all()
+#         for user in users:
+#             selected_posts = random.sample(all_posts, 4)
+#             for post in selected_posts:
+#                 comment = Comment(content=f'Comment from user {user.id} on post {post.id}', timestamp=datetime.now(timezone.utc), user_id=user.id, post_id=post.id)
+#                 session.add(comment)
 
-        session.commit()
-    except Exception as e:
-        session.rollback()
-        raise HTTPException(status_code=500, detail=str(e))
-    finally:
-        session.close()
-    return {"message": "Database populated successfully!"}
+#         session.commit()
+#     except Exception as e:
+#         session.rollback()
+#         raise HTTPException(status_code=500, detail=str(e))
+#     finally:
+#         session.close()
+#     return {"message": "Database populated successfully!"}
 
 app.include_router(auth.router)
 app.include_router(dogs.router)
